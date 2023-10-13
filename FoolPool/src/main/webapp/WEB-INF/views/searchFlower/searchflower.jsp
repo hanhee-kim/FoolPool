63bd18a17438d33594c86bc9214a0a3c229a0cb5
hanhee-kim <914gksl914@naver.com>
2023-10-13 14:28:49 +0900

    searchFlower 구현중

--- FoolPool/src/main/webapp/WEB-INF/views/searchFlower/searchflower.jsp
+++ FoolPool/src/main/webapp/WEB-INF/views/searchFlower/searchflower.jsp
@@ -13,35 +13,37 @@
                         <option value="flowerLang">꽃말</option>
                     </select>
                     <div id="searchFlowerOptionInputBox">
-                    <select class="searchFlowerStartMonth searchFlower_pd_r_22">
+                    <select id="searchFlowerStartMonth" class="searchFlowerStartMonth searchFlower_pd_r_22 searchFlower_OptionDate">
 						<c:set var="n" value="1" />
 						<c:forEach var="n" begin="1" end="12">
 							<option value="${n}">${n<10 ? '0':''}${n}월</option>
 						</c:forEach>
                     </select>
-                    <select class="searchFlowerStartDay searchFlower_pd_r_22">
+                    <select id="searchFlowerStartDay" class="searchFlowerStartDay searchFlower_pd_r_22 searchFlower_OptionDate">
 						<c:set var="n" value="1" />
 						<c:forEach begin="1" end="31">
 							<option value="${n}">${n>9 ? n:'0'}${n>9 ? '':n}일</option>
 							<c:set var="n" value="${n+1}" />
 						</c:forEach>
                     </select>
-<!--                     <div></div> -->
-<!--                     <select class="searchFlowerEndMonth searchFlower_pd_r_22 searchFlower_disabled"> -->
-<%--                         <c:set var="n" value="1" /> --%>
-<%-- 						<c:forEach var="n" begin="1" end="12"> --%>
-<%-- 							<option value="${n}">${n<10 ? '0':''}${n}월</option> --%>
-<%-- 						</c:forEach> --%>
-<!--                     </select> -->
-<!--                     <select class="searchFlowerEndDay searchFlower_pd_r_22 searchFlower_disabled"> -->
-<%--                         <c:set var="n" value="1" /> --%>
-<%-- 						<c:forEach begin="1" end="31"> --%>
-<%-- 							<option value="${n}">${n>9 ? n:'0'}${n>9 ? '':n}일</option> --%>
-<%-- 							<c:set var="n" value="${n+1}" /> --%>
-<%-- 						</c:forEach> --%>
-<!--                     </select> -->
+                    <div id="searchFlowerHyphen" class="searchFlower_disabled">~</div>
+                    <select id="searchFlowerEndMonth" class="searchFlowerEndMonth searchFlower_pd_r_22 searchFlower_OptionDate searchFlower_disabled">
+                        <c:set var="n" value="1" />
+						<c:forEach var="n" begin="1" end="12">
+							<option value="${n}">${n<10 ? '0':''}${n}월</option>
+						</c:forEach>
+                    </select>
+                    <select id="searchFlowerEndDay" class="searchFlowerEndDay searchFlower_pd_r_22 searchFlower_OptionDate searchFlower_disabled">
+                        <c:set var="n" value="1" />
+						<c:forEach begin="1" end="31">
+							<option value="${n}">${n>9 ? n:'0'}${n>9 ? '':n}일</option>
+							<c:set var="n" value="${n+1}" />
+						</c:forEach>
+                    </select>
+                    <input type='text' class='searchFlowerByName searchFlower_disabled' name='searchFlowerByName' id='searchFlowerByName'/>
+                    <input type='text' class='searchFlowerByLang searchFlower_disabled' name='searchFlowerByLang' id='searchFlowerByLang'/>
                     </div>
-                    <button class="searchFlower_searchBtn">검색</button>
+                    <button id="searchFlower_searchBtn" class="searchFlower_searchBtn">검색</button>
                 </div>
                 <div class="flowerCardsection">
 					<div class="flowerCardsection_line">                
--- FoolPool/src/main/webapp/static/js/search_flower.js
+++ FoolPool/src/main/webapp/static/js/search_flower.js
@@ -2,68 +2,83 @@
  * search_flower . js
  * 
  */
+
 //꽃검색 제목 옵션이 선택될 때 
 
 
 $(document).ready(function() {
+	var selectedOption = $("#searchFlowerTitleOption").val();
 	$("#searchFlowerTitleOption").change(function() {
-		var selectedOption = $(this).val();
-		var inputBox = $("#searchFlowerOptionInputBox");
 
-		// 비우기
-		inputBox.empty();
+		// 선택이 되면 모든 요소에 안보이는 클래스 집어넣기
+			
+			$('#searchFlowerStartMonth').addClass("searchFlower_disabled");
+			$('#searchFlowerStartDay').addClass("searchFlower_disabled");
+			$('#searchFlowerHyphen').addClass("searchFlower_disabled");
+			$('#searchFlowerEndMonth').addClass("searchFlower_disabled");
+			$('#searchFlowerEndDay').addClass("searchFlower_disabled");
+			$('#searchFlowerByName').addClass("searchFlower_disabled");
+			$('#searchFlowerByLang').addClass("searchFlower_disabled");
+		
+		// 보이는 클래스 지우기
+			
+			$('#searchFlowerStartMonth').removeClass("searchFlower_abled");
+			$('#searchFlowerStartDay').removeClass("searchFlower_abled");
+			$('#searchFlowerHyphen').removeClass("searchFlower_abled");
+			$('#searchFlowerEndMonth').removeClass("searchFlower_abled");
+			$('#searchFlowerEndDay').removeClass("searchFlower_abled");
+			$('#searchFlowerByName').removeClass("searchFlower_abled");
+			$('#searchFlowerByLang').removeClass("searchFlower_abled");
+		
 
 		// 선택된 옵션에 따라 입력 요소 생성
 		if (selectedOption === "singleDate") {	//날짜(단일)
-			var selectBox = $(`<select class="searchFlowerStartMonth searchFlower_pd_r_22" id="searchFlowerStartMonth">
-						            <c:forEach var="month" begin="1" end="12">
-						                <option value="${month}">${month < 10 ? '0' + month : month}월</option>
-						            </c:forEach>
-						        </select>
-						        <select class="searchFlowerStartDay searchFlower_pd_r_22" id="searchFlowerStartDay">
-						            <c:forEach var="day" begin="1" end="31">
-						                <option value="${day}">${day > 9 ? day : '0' + day}일</option>
-						            </c:forEach>
-						        </select>`);
-
-
-			inputBox.append(selectBox);
+			$('#searchFlowerStartMonth').addClass("searchFlower_abled");
+			$('#searchFlowerStartMonth').removeClass("searchFlower_disabled");
+			$('#searchFlowerStartDay').addClass("searchFlower_abled");
+			$('#searchFlowerStartDay').removeClass("searchFlower_disabled");
 		} else if (selectedOption === "periodDate") {	//날짜(기간)
-			var selectBox = $(`<select class="searchFlowerStartMonth searchFlower_pd_r_22" id="searchFlowerStartMonth">
-									<c:set var="n" value="1" />
-									<c:forEach var="n" begin="1" end="12">
-										<option value="${n}">${n < 10 ? '0' : ''}${n}월</option>
-									</c:forEach>
-			                    </select>
-			                    <select class="searchFlowerStartDay searchFlower_pd_r_22" id="searchFlowerStartDay">
-									<c:set var="n" value="1" />
-									<c:forEach begin="1" end="31">
-										<option value="${n}">${n > 9 ? n : '0'}${n > 9 ? '' : n}일</option>
-										<c:set var="n" value="${n + 1}" />
-									</c:forEach>
-			                    </select>
-			                    <div></div>
-			                    <select class="searchFlowerEndMonth searchFlower_pd_r_22" id="searchFlowerEndMonth">
-			                        <c:set var="n" value="1" />
-									<c:forEach var="n" begin="1" end="12">
-										<option value="${n}">${n < 10 ? '0' : ''}${n}월</option>
-									</c:forEach>
-			                    </select>
-			                    <select class="searchFlowerEndDay searchFlower_pd_r_22" id="searchFlowerEndDay">
-			                        <c:set var="n" value="1" />
-									<c:forEach begin="1" end="31">
-										<option value="${n}">${n > 9 ? n : '0'}${n > 9 ? '' : n}일</option>
-										<c:set var="n" value="${n + 1}" />
-									</c:forEach>
-			                    </select>`);
-			inputBox.append(selectBox);
+			$('#searchFlowerStartMonth').addClass("searchFlower_abled");
+			$('#searchFlowerStartMonth').removeClass("searchFlower_disabled");
+			$('#searchFlowerStartDay').addClass("searchFlower_abled");
+			$('#searchFlowerStartDay').removeClass("searchFlower_disabled");
 
+			$('#searchFlowerHyphen').addClass("searchFlower_abled");
+			$('#searchFlowerHyphen').removeClass("searchFlower_disabled");
+
+			$('#searchFlowerEndMonth').addClass("searchFlower_abled");
+			$('#searchFlowerEndMonth').removeClass("searchFlower_disabled");
+			$('#searchFlowerEndDay').addClass("searchFlower_abled");
+			$('#searchFlowerEndDay').removeClass("searchFlower_disabled");
 		} else if (selectedOption === "flowerName") {	//꽃 이름
-			var inputTextBox = $("<input type='text' class='searchFlowerByName' name='searchFlowerByName' id='searchFlowerByName'/>");
-			inputBox.append(inputTextBox);
+			$('#searchFlowerByName').addClass("searchFlower_abled");
+			$('#searchFlowerByName').removeClass("searchFlower_disabled");
 		} else if (selectedOption === "flowerLang") {	//꽃말
-			var inputTextBox = $("<input type='text' class='searchFlowerByLang' name='searchFlowerByLang' id='searchFlowerByLang'/>");
-			inputBox.append(inputTextBox);
+			$('#searchFlowerByLang').addClass("searchFlower_abled");
+			$('#searchFlowerByLang').removeClass("searchFlower_disabled");
 		}
 	});
+	
+	$('#searchFlower_searchBtn').onclick(function(){
+		var data = {};
+		if(selectedOption === "singleDate"){
+			
+		} else if(selectedOption === "periodDate"){
+			
+		} else if(selectedOption === "flowerName"){
+			
+		} else if(selectedOption === "flowerLang") {
+				
+			}
+		//
+		$.ajax({
+			url:'searchflower',
+			type:'post',
+			data:{},
+			success:'',
+			error:''
+		})//ajax			
+	})
+		
+	
 });
